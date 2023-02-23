3279e53ca85ad39ae4f7ec72a3e0b8d1e3825e0a
Alberto de Olivira <betodeoliveira@outlook.com>
2023-02-22 22:13:48 -0300

    Page trasition animation finished

--- page-transition.js
+++ page-transition.js
@@ -2,12 +2,17 @@
 $(".content-wrapper").addClass("first");
 let menuLinkElement;
 let nextPageLink;
+let navbarLinksColor;
+let transitionType;
 
 $(".navbar_menu-link:not(.w--current").on("click", function (e) {
     // Prevents the link to load the page
     e.preventDefault();
+    // Sets the variables
     menuLinkElement = $(this);
     nextPageLink = $(this).attr("href");
+    navbarLinksColor = $(this).attr("navbar-links-color");
+    transitionType = $(this).attr("transition-type");
     // Grab the content of the next page
     $.ajax({
         url: nextPageLink,
@@ -28,37 +33,59 @@ function pageTransition() {
         paused: false,
         onComplete: updatePage
     });
-    pageTransitionTl.to(".content-wrapper.first", {
-        opacity: 0,
-        duration: 0.5,
-        ease: "power1.out"
-    });
-    pageTransitionTl.fromTo(".content-wrapper.second", {
-        opacity: 0
-    },
-        {
-            opacity: 1,
+    // Plays a different animation deppending on the transition type attribute
+    if (transitionType == "slide-from-bottom") {
+        $(".content-wrapper.second").css("z-index", "3");
+        pageTransitionTl.from(".content-wrapper.second", {
+            y: "110vh",
+            delay: 0.2,
+            duration: 0.8,
+            ease: "power2.out"
+        });
+        pageTransitionTl.to(
+            ".content-wrapper.first",
+            {
+                scale: 0.7,
+                duration: 0.3,
+                ease: "power1.out"
+            },
+            0
+        );
+    }
+    // Fade
+    else {
+        pageTransitionTl.to(".content-wrapper.first", {
+            opacity: 0,
             duration: 0.5,
             ease: "power1.out"
-        }, 0);
+        });
+        pageTransitionTl.fromTo(".content-wrapper.second", {
+            opacity: 0
+        },
+            {
+                opacity: 1,
+                duration: 0.5,
+                ease: "power1.out"
+            }, 0);
+    }
+    // Adds the current effect to the cliked link
     pageTransitionTl.to(menuLinkElement, {
         opacity: 0.5,
         duration: 0.5,
         ease: "power1.out"
     }, 0);
-
-    if ($(".content-wrapper.second").find(".section_contact-about")) {
-        pageTransitionTl.to(".page-background", {
-            backgroundColor: "#FFFFFF",
-            duration: 0.5,
-            ease: "power1.out"
-        }, 0);
-        pageTransitionTl.to(".navbar_menu-link", {
-            color: "#2400ff",
-            duration: 0.5,
-            ease: "power1.out"
-        }, 0);
-    }
+    // Removes the current event from the actual link
+    pageTransitionTl.to($(".navbar_menu-link.w--current"), {
+        opacity: 1,
+        duration: 0.5,
+        ease: "power1.out"
+    }, 0);
+    // Changes the navbar link colors
+    pageTransitionTl.to(".navbar_menu-link", {
+        color: navbarLinksColor,
+        duration: 0.5,
+        ease: "power1.out"
+    }, 0);
 }
 
 function updatePage() {
