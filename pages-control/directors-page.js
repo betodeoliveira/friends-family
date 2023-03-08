1d0e3f0d7233798af5e6fb657f55cbc90bd78a24
Alberto de Olivira <betodeoliveira@outlook.com>
2023-03-08 17:37:44 -0300

    Testing

--- pages-control/directors-page.js
+++ pages-control/directors-page.js
@@ -33,99 +33,99 @@ $(".director_item-wrapper").each(function (index) {
 //#endregion
 
 //#region [ Item Click ]
-let directorPageLink;
-let directorPageBackgroundColor;
-let directorPageNavbarLinksColor;
-let directorTitleMove;
-// What happens when a director name is clicked
-$(".director_item-button").on("click", function (e) {
-    // Prevents the link to load the page
-    e.preventDefault();
-    // Sets the title that will Flip to the selected position
-    directorTitleMove = $(this).find(".director_item-title-move").addClass("selected");
-    // Sets the variables
-    directorPageLink = $(this).attr("href");
-    directorPageBackgroundColor = $(this).attr("page-background-color");
-    directorPageNavbarLinksColor = $(this).attr("navbar-links-color");
+// let directorPageLink;
+// let directorPageBackgroundColor;
+// let directorPageNavbarLinksColor;
+// let directorTitleMove;
+// // What happens when a director name is clicked
+// $(".director_item-button").on("click", function (e) {
+//     // Prevents the link to load the page
+//     e.preventDefault();
+//     // Sets the title that will Flip to the selected position
+//     directorTitleMove = $(this).find(".director_item-title-move").addClass("selected");
+//     // Sets the variables
+//     directorPageLink = $(this).attr("href");
+//     directorPageBackgroundColor = $(this).attr("page-background-color");
+//     directorPageNavbarLinksColor = $(this).attr("navbar-links-color");
     
-    // Grabs the content of the next page
-    $.ajax({
-        url: directorPageLink,
-        success: function (response) {
-            let element = $(response).find(".content-wrapper").addClass("second");
-            $(".main-wrapper").append(element);
-        },
-        complete: function () {
-            playDirectorFlip();
-            playDirectorLeave();
-        }
-    });
-});
+//     // Grabs the content of the next page
+//     $.ajax({
+//         url: directorPageLink,
+//         success: function (response) {
+//             let element = $(response).find(".content-wrapper").addClass("second");
+//             $(".main-wrapper").append(element);
+//         },
+//         complete: function () {
+//             playDirectorFlip();
+//             playDirectorLeave();
+//         }
+//     });
+// });
 
-function playDirectorFlip() {
-    console.log("play flip");
-    let directorMoveTitleState = Flip.getState(directorTitleMove);
-    $(directorTitleMove).appendTo($(".div-block-13"));
-    Flip.from(directorMoveTitleState, {
-        duration: 0.5,
-        ease: "power1.out"
-    });
-    setTimeout(() => {
-        gotToNextPage();
-    }, 500);
-}
+// function playDirectorFlip() {
+//     console.log("play flip");
+//     let directorMoveTitleState = Flip.getState(directorTitleMove);
+//     $(directorTitleMove).appendTo($(".div-block-13"));
+//     Flip.from(directorMoveTitleState, {
+//         duration: 0.5,
+//         ease: "power1.out"
+//     });
+//     setTimeout(() => {
+//         gotToNextPage();
+//     }, 500);
+// }
 
-function playDirectorLeave() {
-    $("html").addClass("animating");
-    // Creates the timeline
-    directorLeaveTimeline = gsap.timeline({
-        paused: false,
-        onComplete: playFlip
-    });
+// function playDirectorLeave() {
+//     $("html").addClass("animating");
+//     // Creates the timeline
+//     directorLeaveTimeline = gsap.timeline({
+//         paused: false,
+//         onComplete: playFlip
+//     });
 
-    // The content shouldn't be clickable
-    $(".content-wrapper.first").css("pointer-events", "none");
+//     // The content shouldn't be clickable
+//     $(".content-wrapper.first").css("pointer-events", "none");
 
-    // Hides the current content
-    directorLeaveTimeline.to(".content-wrapper.first", {
-        opacity: 0,
-        duration: 0.3,
-        ease: "power1.out"
-    });
+//     // Hides the current content
+//     directorLeaveTimeline.to(".content-wrapper.first", {
+//         opacity: 0,
+//         duration: 0.3,
+//         ease: "power1.out"
+//     });
 
-    // Changes the background color
-    directorLeaveTimeline.to(".page-background-color", {
-        backgroundColor: nextPageBackgroundColor2,
-        duration: 0.3,
-        ease: "power1.out"
-    }, 0);
+//     // Changes the background color
+//     directorLeaveTimeline.to(".page-background-color", {
+//         backgroundColor: nextPageBackgroundColor2,
+//         duration: 0.3,
+//         ease: "power1.out"
+//     }, 0);
 
-    // Changes the navbar link colors
-    directorLeaveTimeline.to(".navbar_menu-link", {
-        color: nextPageNavbarLinksColor2,
-        duration: 0.5,
-        ease: "power1.out"
-    }, 0);
+//     // Changes the navbar link colors
+//     directorLeaveTimeline.to(".navbar_menu-link", {
+//         color: nextPageNavbarLinksColor2,
+//         duration: 0.5,
+//         ease: "power1.out"
+//     }, 0);
 
-    // Animates the next page content
-    $(".content-wrapper.second").css("z-index", "3");
-    directorLeaveTimeline.fromTo(".content-wrapper.second", {
-        opacity: 0
-    }, {
-        opacity: 1,
-        duration: 0.5,
-        ease: "power1.out"
-    }, 0);
-}
+//     // Animates the next page content
+//     $(".content-wrapper.second").css("z-index", "3");
+//     directorLeaveTimeline.fromTo(".content-wrapper.second", {
+//         opacity: 0
+//     }, {
+//         opacity: 1,
+//         duration: 0.5,
+//         ease: "power1.out"
+//     }, 0);
+// }
 
-function gotToNextPage() {
-    window.location = directorPageLink;
-}
+// function gotToNextPage() {
+//     window.location = directorPageLink;
+// }
 
-// Realods the page on back
-window.onpageshow = function (event) {
-    if (event.persisted) {
-        window.location.reload();
-    }
-};
+// // Realods the page on back
+// window.onpageshow = function (event) {
+//     if (event.persisted) {
+//         window.location.reload();
+//     }
+// };
 //#endregion
