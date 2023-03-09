5b19e357999d97762c6b7c397578a828ac58ff5c
Alberto de Olivira <betodeoliveira@outlook.com>
2023-03-09 19:56:47 -0300

    Work Control

--- pages-control/work-control.js
+++ pages-control/work-control.js
@@ -2,7 +2,7 @@
 const viewportWidth = (coef) => window.innerWidth * (coef / 100);
 
 // Hides the work item when it reaches the top of the screen
-$(".work_item-button").each(function (index) {
+$(".works_item-button").each(function (index) {
     ScrollTrigger.create({
         trigger: $(this),
         start: () => 'top ' + Math.min(viewportWidth(10)) + ' top',
@@ -11,39 +11,57 @@ $(".work_item-button").each(function (index) {
         onEnter: () => {
             gsap.to($(this), { x: "-4rem", ease: "power2.out", duration: 0.5 });
             gsap.to($(this), { opacity: 0, duration: 0.25 });
-            gsap.set((this), {pointerEvents: "none"});
+            gsap.set((this), { pointerEvents: "none" });
         },
         onEnterBack: () => {
             gsap.to($(this), { x: "0rem", ease: "power2.out", duration: 0.5 });
             gsap.to($(this), { opacity: 1, duration: 0.5 });
-            gsap.set((this), {pointerEvents: "auto"});
+            gsap.set((this), { pointerEvents: "auto" });
         }
     });
 });
 //#endregion
 
 //#region [ Item Hover ]
-$(".work_item-wrapper").each(function (index) {
-    let workItemThumbComponent = $(this).find(".work_item-thumb-component");
-    let workItemThumbPlayer = new Plyr($(this).find(".plyr_thumb")[0], {
+$(".works_item-wrapper").each(function (index) {
+    let workItemButton = $(this).find(".works_item-button");
+    let thumbComponent = $(this).find(".works_item-thumb-component");
+    let thumbPlayer = new Plyr($(this).find(".plyr_thumb")[0], {
         controls: [],
         resetOnEnd: true
     });
 
+    let thumbTimeline = gsap.timeline({
+        paused: true,
+        onReverseComplete: hideAboutComponent
+    });
+
+    thumbTimeline.from(thumbComponent, {
+        opacity: 0,
+        duration: 0.5,
+        ease: "power1.out"
+    })
+
     // Shows animated thumb on hover
     $(workItemButton).on("mouseenter", function () {
-        // console.log("hovering item");
-        $(workItemThumbComponent).css("opacity", "1");
-        workItemThumbPlayer.volume = 0;
-        workItemThumbPlayer.muted = true;
-        workItemThumbPlayer.play();
+        if ($(thumbComponent).css("display") == "none") {
+            $(thumbComponent).css("display", "block");
+            workItemThumbPlayer.volume = 0;
+            workItemThumbPlayer.muted = true;
+            workItemThumbPlayer.play();
+            thumbTimeline.play();
+        };
     });
 
     // Hides animated thumb on leave
     $(workItemButton).on("mouseleave", function () {
-        // console.log("hovering out item");
-        $(workItemThumbComponent).css("opacity", "0");
-        workItemThumbPlayer.stop();
+        thumbTimeline.reverse();
+        workItemThumbPlayer.pause();
     });
 });
+
+function hideThumbComponent() {
+    $(thumbComponent).css("display", "none");
+    workItemThumbPlayer.stop();
+}
 //#endregion
