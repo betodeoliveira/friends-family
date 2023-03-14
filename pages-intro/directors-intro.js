44cc5aff1eb2cc85a9ccecc60f874df4e8d6d5a1
Alberto de Olivira <betodeoliveira@outlook.com>
2023-03-14 17:18:51 -0300

    Directors intro mobile

--- pages-intro/directors-intro.js
+++ pages-intro/directors-intro.js
@@ -1,20 +1,35 @@
-let directorsIntroTimeline = gsap.timeline({
-    paused: false,
-    onComplete: enablePageEvents
+$(document).ready(function () {
+    let matchMedia = gsap.matchMedia();
+
+    let introTimeline = gsap.timeline({
+        paused: false,
+        onComplete: enablePageEvents
+    });
+
+    // Disables page events at first
+    $(".content-wrapper").css("pointer-events", "none");
+    $(".content-wrapper").css("opacity", "1");
+
+    // Desktop Match Media
+    matchMedia.add("(min-width: 992px)", () => {
+        setIntroTimeline("random");
+    });
+
+    // Tablet and below Match Media
+    matchMedia.add("(max-width: 991px)", () => {
+        setIntroTimeline("start");
+    });
+
+    function setIntroTimeline(staggerFrom) {
+        introTimeline.from($(".director_item-wrapper"), {
+            opacity: 0,
+            duration: 0.5,
+            ease: "power1.out",
+            stagger: { amount: 1, from: staggerFrom }
+        });
+    }
+
+    function enablePageEvents() {
+        $(".content-wrapper").css("pointer-events", "auto");
+    }
 });
-
-// Disables page events at first
-$(".content-wrapper").css("pointer-events", "none");
-$(".content-wrapper").css("opacity", "1");
-
-// Configurates the timeline
-directorsIntroTimeline.from($(".director_item-wrapper"), {
-    opacity: 0,
-    duration: 0.5,
-    ease: "power1.out",
-    stagger: { amount: 1, from: "random" }
-});
-
-function enablePageEvents() {
-    $(".content-wrapper").css("pointer-events", "auto");
-}
