2f03e332f4ed114741b5420a45a85960ecde31b3
Alberto de Olivira <betodeoliveira@outlook.com>
2023-03-21 16:04:53 -0300

    Directors template intro

--- pages-intro/directors-template-intro.js
+++ pages-intro/directors-template-intro.js
@@ -5,7 +5,6 @@ let directorsTemplateIntroTimeline = gsap.timeline({
 
 // Disables page events at first
 $(".content-wrapper").css("pointer-events", "none");
-$(".content-wrapper").css("opacity", "1");
 
 // Configurates the timeline
 directorsTemplateIntroTimeline.fromTo(".director-works_collection", {
@@ -22,4 +21,6 @@ function enablePageEvents() {
     $(".content-wrapper").css("pointer-events", "auto");
     // GSAP add some styles that must be removed to the thumb player and video player work as expected
     $(".director-works_collection").removeAttr("style");
+    // If we set the contect to opacity 0 when the page loads the name o director will blink, so we hide the collection and after removing the style set it to opacoty 1
+    $(".director-works_collection").css("opacity", "1");
 }
